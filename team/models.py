10ce7c849f00ad1e07cfd6fba3f75172e809dc6f
mr8bit <info@mrbyte.ru>
2017-03-10 00:02:35 +0300

    Настройка пользователя

--- team/models.py
+++ team/models.py
@@ -26,26 +26,22 @@ class SocialNetwork(models.Model):
 
 
 class TeamManager(BaseUserManager):
-    def create_user(self, first_name, email, last_name, password=None):
+    def create_user(self,  email,  password=None):
         if not email:
             raise ValueError('Users must have an email address')
         user = self.model(
             email=self.normalize_email(email),
-            last_name=last_name,
-            first_name=first_name
+
         )
 
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, first_name, last_name,  password):
+    def create_superuser(self, email,   password):
         user = self.create_user(
             email,
-            password,
-            last_name,
-            first_name
-        )
+            password=password,  )
         user.is_admin = True
         user.save(using=self._db)
         return user
@@ -76,17 +72,19 @@ class Member(AbstractBaseUser):
     objects = TeamManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['first_name', 'last_name']
 
     class Meta:
         verbose_name = "Участника"
         verbose_name_plural = "Участники"
 
     def get_full_name(self):
-        return self.first_name + ' ' + self.last_name
+        if self.first_name:
+            return self.first_name + ' ' + self.last_name
+        else:
+            return self.email
 
     def __str__(self):
-        return self.first_name + ' ' + self.last_name
+        return self.get_full_name
 
     def get_short_name(self):
         return self.first_name
