908829010bde556b9dec164291ca1b6d533d1055
mr8bit <info@mrbyte.ru>
2017-03-10 00:16:49 +0300

    Настройка пользователя

--- team/models.py
+++ team/models.py
@@ -78,7 +78,7 @@ class Member(AbstractBaseUser):
         verbose_name_plural = "Участники"
 
     def get_full_name(self):
-        if self.first_name:
+        if self.first_name and self.last_name :
             return self.first_name + ' ' + self.last_name
         else:
             return self.email
