bf7584663f9944fad555cc5a95574fa76e84c911
mr8bit <info@mrbyte.ru>
2017-03-10 00:14:46 +0300

    Настройка пользователя

--- team/models.py
+++ team/models.py
@@ -84,7 +84,10 @@ class Member(AbstractBaseUser):
             return self.email
 
     def __str__(self):
-        return self.get_full_name
+        if self.first_name and self.last_name :
+            return self.first_name + ' ' + self.last_name
+        else:
+            return self.email
 
     def get_short_name(self):
         return self.first_name
