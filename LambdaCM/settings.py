99f1366eaedba01e3d5ed0fab66aecd55e6df36c
mr8bit <info@mrbyte.ru>
2017-03-11 01:20:08 +0300

    Перещены все STATIC и MEDIA файлы в корень файл менеджера для удобной работы с ними в будущем

--- LambdaCM/settings.py
+++ LambdaCM/settings.py
@@ -155,11 +155,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'media/static'),)
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-FILER_CANONICAL_URL = os.path.join(MEDIA_ROOT, 'sharing/')
 
 FILEBROWSER_DIRECTORY = ''
 DIRECTORY = ''
